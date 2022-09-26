d7d5b6c2b98e2020b2fd22c5b165095e096d6182
Vladislav Sabenin <vladislav.sabenin@interactivestandard.com>
2022-09-26 16:16:25 +0700

    Реализовано сохранение чатов внутри TelegramClient

--- src/service/telegram_client.py
+++ src/service/telegram_client.py
@@ -7,6 +7,7 @@ class TelegramClient:
     def __init__(self, session: ClientSession, **kwargs):
         self.__path = f"https://api.telegram.org/bot{kwargs.get('token')}"
         self.__session = session
+        self.__chats_ids = set()
 
     async def get_chat_ids(self, default_chat_id: int) -> Set[int]:
         async with self.__session.get(url=f"{self.__path}/getUpdates") as response:
@@ -16,7 +17,8 @@ class TelegramClient:
             if not seq_msgs:
                 return {default_chat_id}
             chat_ids = [msg.get('message', {}).get('chat', {}).get('id', None) for msg in seq_msgs]
-            return set(_id for _id in chat_ids if _id is not None)
+            self.__chats_ids |= set(_id for _id in chat_ids if _id is not None)
+            return self.__chats_ids
 
     async def send_msg2chat(self, chat_id: int, msg: str):
         return await self.__session.post(url=f"{self.__path}/sendMessage",
